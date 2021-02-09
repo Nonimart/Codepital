cd1e1ca7fb7fe0a2ccde9c9b1ebf88239472fa23
Antoine <martinon.toinou@protonmail.com>
2021-02-09 15:47:09 +0100

    initial commit

---
+++ public/js/class_lieu.js
@@ -0,0 +1,9 @@
+export default class Lieu {
+    constructor(icon, nom,visiteurs,  paniers,stock){
+        // this.icon = icon
+        // this.nom = nom
+        // this.visiteurs = visiteurs
+        // this.paniers = paniers
+        // this.stock = stock
+    }
+}
